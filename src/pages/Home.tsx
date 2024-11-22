a2ebd68b82a2c212f8cbf3854d8850335c6734ab
tysonwhyy <128116726+tysonwhyy@users.noreply.github.com>
2024-11-21 19:01:56 -0500

    edited home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,25 @@
-import Nav2 from "../components/Nav2.tsx";
+import React, { useState } from "react";
+import Nav2 from "../components/Nav2";
+import styles from "./home.module.css";
+import searchNormalIcon from "../assets/search-normal.png";
+
 export default function Home() {
+  const [input, setInput] = useState("");
+
   return (
     <>
       <Nav2 />
+      <div className={styles.searchContainer}>
+        <input
+          className={styles.searchInput}
+          placeholder="Search for a hobby group"
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </div>
+      <div></div>
+      <div></div>
     </>
   );
 }
